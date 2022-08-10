50a369bc51970172bb59bbfefe007d52daf6bdf2
Gamal-Mohammed <109288005+Gamal-Mohammed@users.noreply.github.com>
2022-08-10 19:42:55 +0200

    test

---
+++ task 1/dynimicTable.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+<?php
+// dynamic table
+// dynamic rows //4 
+// dynamic columns // 4
+// check if gender of user == m ==> male // 1
+// check if gender of user == f ==> female // 1
+
+$users = [
+    (object)[
+        'id' => 1,
+        'name' => 'ahmed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'football', 'swimming', 'running'
+        ],
+        'activities' => [
+            "school" => 'drawing',
+            'home' => 'painting'
+        ],
+        'phones'=>[012312312,1231513513,89746543],
+    ],
+    (object)[
+        'id' => 2,
+        'name' => 'mohamed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'swimming', 'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ],
+        'phones'=>[1231513513,89746543],
+    ],
+    (object)[
+        'id' => 3,
+        'name' => 'menna',
+        "gender" => (object)[
+            'gender' => 'f'
+        ],
+        'hobbies' => [
+            'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ],
+        'phones'=>[],
+    ],  
+];
+
+?>
+
+
+
+
+
+</body>
+</html>
