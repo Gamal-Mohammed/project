fd2b2eedce3cd4286e5122fe7e5d98668d496b2e
Gamal-Mohammed <109288005+Gamal-Mohammed@users.noreply.github.com>
2022-08-14 09:29:04 +0200

    Update Result.php

--- task 2/Result.php
+++ task 2/Result.php
@@ -6,3 +6,24 @@ session_start();
 echo $_SESSION['resu']."  ".$_SESSION['test'];
 /////header("location:Result.php");die;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<label for="">Are you satisfied with the level of cleanliness?</label>
+<label for=""></label>
+<label for="">Are you satisfied with the prices of the services?</label>
+<label for=""></label>
+<label for="">Are you satisfied with the nursing service</label>
+<label for=""></label>
+<label for="">Are you satisfied with the level of the doctor?</label>
+<label for=""></label>
+<label for="">Are you satisfied with the calmness in the hospital?</label>
+<label for=""></label>
+</body>
+</html>
