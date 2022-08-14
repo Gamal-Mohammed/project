3505d7a87a411335b872d745889f0b41ecd82d17
Gamal-Mohammed <109288005+Gamal-Mohammed@users.noreply.github.com>
2022-08-14 20:58:03 +0200

    Add files via upload

--- task 2/Number.php
+++ task 2/Number.php
@@ -14,16 +14,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
-    <title>Document</title>
+    <title>Hospital</title>
 </head>
 
 <body>
   
      <div class="container">
         <br>
-        <h1>calc</h1>
+        <h1>Hospital</h1>
+        <br>
     <form  >
-<input type="text" name="number2" class="form-control">
+<input type="text" name="number2" class="form-control" placeholder="enter phone number">
 <br>
 
 <br>
